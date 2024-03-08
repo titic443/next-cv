ee8b3dbfb2caecdc50537b3d480dc2fe3dc347e1
titi.ea <titi.cha@energyabsolute.co.th>
2024-03-08 10:15:32 +0700

    menu button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,16 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-12 lg:px-20">
       {/* Logo */}
@@ -15,7 +25,31 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div>2</div>
+      {/* Menu button */}
+      <div>
+        <button
+          className="w-10 h-8 flex flex-col justify-between  z-50 relative"
+          onClick={() =>
+            setOpen((prev) => {
+              console.log(prev);
+              return !prev;
+            })
+          }
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+        </button>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
